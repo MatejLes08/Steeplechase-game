e3c53bdf10c8c6c2e6f9f758306d2a6d7316c1af
SimeonK <simotv21@gmail.com>
2025-04-08 22:33:35 +0200

    pridanie suboru Game.py

---
+++ Game.py
@@ -0,0 +1,88 @@
+import time
+import random
+
+class Game:
+    def __init__(self, update_ui_callback, update_record_callback):
+        self.prejdene_metre = 0
+        self.draha = 2000
+        self.kon_rychlost = 0
+        self.kon_max_rychlost = 60
+        self.kon_vydrz = 100
+        self.zataz = 0
+        self.sila = 100
+
+        self.cas = 0
+        self.minuty = 0
+
+        self.update_ui = update_ui_callback
+        self.update_record = update_record_callback
+
+    def zisti_pasmo(self, ostava, mnp, msp, n1, n2, n3):
+        oddych_cis = 0.01
+        zrychlenie = 1
+        narocnost = 7000
+        bonus = 1
+ 
+        if mnp >= ostava >= mnp - 300:
+            narocnost = 5000
+            oddych_cis = 0.005
+
+        elif msp >= ostava >= msp - 400:
+            zrychlenie = 1.25
+
+        if any(n >= ostava >= n - 20 for n in [n1, n2, n3]):
+            bonus = 10
+
+        return oddych_cis, zrychlenie, narocnost, bonus
+
+    def start_race(self):
+        self.prejdene_metre = 0
+        self.cas = 0
+        self.minuty = 0
+        self.zataz = 0
+        self.sila = 100
+        self.kon_rychlost = 0
+
+        mnp, msp, n1, n2, n3 = self.generacia_terenu()
+        ostava = self.draha
+
+        while self.prejdene_metre < self.draha:
+            time.sleep(0.01)
+
+            self.cas += 0.01
+            if int(self.cas) >= 60:
+                self.cas -= 60
+                self.minuty += 1
+
+            oddych_cis, zrychlenie, narocnost, bonus = self.zisti_pasmo(ostava, mnp, msp, n1, n2, n3)
+
+            if self.kon_rychlost == 0:
+                if self.sila < 100:
+                    self.zataz -= oddych_cis * 2 * bonus
+
+            elif self.sila <= 0:
+                self.kon_rychlost = 4
+
+            elif self.kon_rychlost <= 12:
+                if self.sila < 100:
+                    self.zataz -= oddych_cis
+
+            elif self.kon_rychlost <= 24:
+                self.zataz += self.kon_rychlost / narocnost
+
+            elif self.kon_rychlost < 50:
+                self.zataz += self.kon_rychlost / (narocnost - 2000)
+
+            else:
+                self.zataz += self.kon_rychlost / (narocnost - 3000)
+
+            self.sila = self.kon_vydrz - self.zataz
+            self.prejdene_metre += self.kon_rychlost * zrychlenie / 3.6 * 0.01
+            ostava = round(self.draha - self.prejdene_metre)
+
+            self.update_ui(int(self.kon_rychlost * zrychlenie), ostava, int(self.sila))
+
+        cas_str = f"{self.minuty}:{int(self.cas):02d}"
+        self.ulozit_cas(cas_str)
+        self.update_record(self.najnizsi_cas())
+
