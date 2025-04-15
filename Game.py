4440a1909c9de99a6845c91586d9024e831958bc
Matej_Lesko <mtjlesko@gmail.com>
2025-04-15 22:34:54 +0200

    Ďalšie menšie úpravy

--- Game.py
+++ Game.py
@@ -7,6 +7,8 @@ class Game:
     def __init__(self, update_ui_callback, update_record_callback):
         self.DRAHA = 2000
         self.KON_VYDRZ = 100
+        self.NAROCNOST_BEH = 2000
+        self.NAROCNOST_SPRINT = 3000
         self.prejdene_metre = 0
         self.kon_rychlost = 0
         self.zataz = 0
@@ -23,12 +25,6 @@ class Game:
         self.bezi = False 
 
     def start_race(self):
-        self.prejdene_metre = 0
-        self.cas = 0
-        self.minuty = 0
-        self.zataz = 0
-        self.sila = 100
-        self.kon_rychlost = 0
         self.bezi = True 
 
         ostava = self.DRAHA
@@ -44,24 +40,24 @@ class Game:
             oddych_cis, zrychlenie, narocnost, bonus, typ_terenu = self.terrain.zisti_pasmo(ostava)
 
             if self.kon_rychlost == 0:
-                if self.sila < 100:
+                if self.sila < self.KON_VYDRZ:
                     self.zataz -= oddych_cis * 2 * bonus
 
             elif self.sila <= 0:
                 self.kon_rychlost = 4
 
             elif self.kon_rychlost <= 12:
-                if self.sila < 100:
+                if self.sila < self.KON_VYDRZ:
                     self.zataz -= oddych_cis
 
             elif self.kon_rychlost <= 24:
                 self.zataz += self.kon_rychlost / narocnost
 
             elif self.kon_rychlost < 50:
-                self.zataz += self.kon_rychlost / (narocnost - 2000)
+                self.zataz += self.kon_rychlost / (narocnost - self.NAROCNOST_BEH)
 
             else:
-                self.zataz += self.kon_rychlost / (narocnost - 3000)
+                self.zataz += self.kon_rychlost / (narocnost - self.NAROCNOST_SPRINT)
 
             self.sila = self.KON_VYDRZ - self.zataz         # odoberanie energie kona
             self.prejdene_metre += self.kon_rychlost * zrychlenie / 3.6 * 0.01 #obnovovanie prejdených metrov
