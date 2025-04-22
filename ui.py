8713b1b090c8f7aba4310c916fc46463da773116
Matej_Lesko <mtjlesko@gmail.com>
2025-04-22 19:33:38 +0200

    Opravil som spomalenie času

--- ui.py
+++ ui.py
@@ -96,11 +96,12 @@ class UI:
 
     def run(self):
         clock = pygame.time.Clock()
+        dt = 0.0 # dt nastavujem najprv na 0.0, ale potom ho zmením - len, aby to nespadlo na chybe referenced before assignment
         running = True
         while running:
-            dt = clock.tick(60) / 1000  # dt v sekundách
             running = self.handle_events()
             if self.game:
                 self.game.update(dt)
             self.draw_ui()
+            dt = clock.tick(60) / 1000  # dt v sekundách
         pygame.quit()
