35f2694e3c2c30e99ed6a732921304e9165b1b6a
BarrLeaf <vladovasil2929@gmail.com>
2025-04-29 19:33:44 +0200

    Oprava ukladania rekordu

--- Utils.py
+++ Utils.py
@@ -1,16 +1,21 @@
 class Utils:
     @staticmethod
     def cas_na_stotiny(cas_str):
-        """Prevod formátu MM:SS:SS (minúty, sekundy, stotiny) na počet stotín"""
+        parts = cas_str.split(":")
+        if len(parts) == 3:
+            minuty, sekundy, stotiny = parts
+        elif len(parts) == 2:
+            minuty, sekundy = parts
+            stotiny = "0"
+        else:
+            return 0
         try:
-            minuty, sekundy, stotiny = cas_str.split(":")
             return int(minuty) * 6000 + int(sekundy) * 100 + int(stotiny)
         except ValueError:
             return 0
 
     @staticmethod
     def stotiny_na_cas(stotiny):
-        """Prevod stotín na formát MM:SS:SS (napr. 1:09:84)"""
         minuty = stotiny // 6000
         zvysok = stotiny % 6000
         sekundy = zvysok // 100
@@ -38,3 +43,7 @@ class Utils:
             return Utils.stotiny_na_cas(najnizsi)
         else:
             return "N/A"
+    @staticmethod
+    def map_range(value, from_min, from_max, to_min, to_max):
+        # Prevod na nový rozsah
+        return (value - from_min) * (to_max - to_min) / (from_max - from_min) + to_min
